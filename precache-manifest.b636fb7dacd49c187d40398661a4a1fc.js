2511bc3dc0ffc03dd9ff46365bd5c5f5d3087485
Alex Loburev <48661813+Beaterok@users.noreply.github.com>
2023-04-03 19:20:51 -0400

    Updates

---
+++ precache-manifest.b636fb7dacd49c187d40398661a4a1fc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "45fae48bd34d58a7b1a9",
+    "url": "/React_Portfolio/static/js/main.45fae48b.chunk.js"
+  },
+  {
+    "revision": "1d32dbbbc6ef542863e1",
+    "url": "/React_Portfolio/static/js/1.1d32dbbb.chunk.js"
+  },
+  {
+    "revision": "0b3b1d1736f2c5113254",
+    "url": "/React_Portfolio/static/js/runtime~main.0b3b1d17.js"
+  },
+  {
+    "revision": "43f0f26a5bdd6632f5220f659ce2e31b",
+    "url": "/React_Portfolio/static/media/me.43f0f26a.jpg"
+  },
+  {
+    "revision": "1001baa6c08ed971c009cb61deb3327a",
+    "url": "/React_Portfolio/static/media/Marvel-Search.1001baa6.gif"
+  },
+  {
+    "revision": "c3907c33410a67fe04d1b4db5e7c119e",
+    "url": "/React_Portfolio/static/media/JATE.c3907c33.png"
+  },
+  {
+    "revision": "1cb33479a3d84dfe88eb73fd13e30f43",
+    "url": "/React_Portfolio/static/media/Budget_Buddy.1cb33479.png"
+  },
+  {
+    "revision": "fc785c6a999f863ad57c223642d5de79",
+    "url": "/React_Portfolio/static/media/webscreenshot.fc785c6a.png"
+  },
+  {
+    "revision": "36aea3d0560b3d127997e42b3547c26c",
+    "url": "/React_Portfolio/static/media/Your-Thoughts.36aea3d0.png"
+  },
+  {
+    "revision": "031038d8a83b0df60a934fa50f018935",
+    "url": "/React_Portfolio/static/media/Resume.031038d8.docx"
+  },
+  {
+    "revision": "8123811b853181e460a526ba6ea10cea",
+    "url": "/React_Portfolio/index.html"
+  }
+];
